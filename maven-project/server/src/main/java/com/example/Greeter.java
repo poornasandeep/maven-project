40beeb6b6bfc4f6af6bbeb855bf37167d31459a2
unknown <you@example.com>
2017-12-29 20:47:35 +0530

    My First Message

---
+++ maven-project/server/src/main/java/com/example/Greeter.java
@@ -0,0 +1,26 @@
+
+package com.example;
+
+
+/**
+ * This is a class.
+ */
+public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+
+  }
+
+  /*
+
+	This is sandeep
+ */
+  public final String greet(final String someone) {
+
+	
+    return String.format("Hello, %s!", someone);
+  }
+}
